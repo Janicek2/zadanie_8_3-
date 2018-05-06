06e7dfb42f355545fd34cd9cb70d3b5391f06309
Janicek2 <bjanicki82@gmail.com>
2018-05-06 17:11:07 +0200

    update declaration and 0 bug

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-var 
-    a = 3,
-    b = 4,
-    value = (a * a) - (2 * a * b) - (b * b);
+var a = 0;
+var b = 0;
+var value = (a * a) - (2 * a * b) - (b * b);
+var isItpositive = value < 0 ? 'Zmienna jest ujemna' : 'Zmienna jest dodatnia';
 
-console.log('Zmienna a: ' + a + ' i zmienna b: ' + b + ' po wykonaniu zadania (a * a) - (2 * a * b) - (b * b) daje wynik: ' + value);
-
-var isItpositive = value <= 0 ? 'Zmienna jest ujemna' : 'Zmienna jest dodatnia';
-
-console.log (isItpositive);
+console.log('Zmienna a: ' + a + ' i zmienna b: ' + b + ' po wykonaniu zadania (a * a) - (2 * a * b) - (b * b) daje wynik: ' + value 
++ ' ' + isItpositive);
