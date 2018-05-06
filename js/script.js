81d149147a862fe3f43faed6f8bc902a9f1a937b
Janicek2 <bjanicki82@gmail.com>
2018-05-06 09:38:51 +0200

    Add comment in js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+// var name = prompt('Enter your name');
+/*alert('Hello, ' + name);
+console.log('Hello, ' + name);*/
+
+// Komentarz dodane
+
+/* Jakby były takie zadania to pewnie bym nadrobił a tak może być cięćko ;-) */
