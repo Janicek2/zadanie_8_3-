63c545c92da127f3a37fbbc9bbd02987cc8baf70
Janicek2 <bjanicki82@gmail.com>
2018-05-07 06:49:45 +0200

    Fixed bug zero

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var a = 0;
 var b = 0;
 var value = (a * a) - (2 * a * b) - (b * b);
-var isItpositive = value < 0 ? 'Zmienna jest ujemna' : 'Zmienna jest dodatnia';
+
+if ( value > 0 ) {
+    var isItpositive = 'Liczba większa od zera'
+} else if ( value < 0 ) {
+    var isItpositive = 'Liczba mniejsza od zera'
+} else {
+    var isItpositive = 'Liczba to zero'
+}
+
+
 
 console.log('Zmienna a: ' + a + ' i zmienna b: ' + b + ' po wykonaniu zadania (a * a) - (2 * a * b) - (b * b) daje wynik: ' + value 
 + ' ' + isItpositive);
